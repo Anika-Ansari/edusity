46ad0ecaefdcb959cc0105e309289ba6f09ebdde
Anika-Ansari <anikajameel786@gmail.com>
2025-08-21 22:58:15 +0500

    Added Code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
 
 function App() {
   return (
@@ -14,6 +16,12 @@ function App() {
         <Title subTitle='OUR PROGRAM' title='What We Offer'/>
         <Programs/>
         <About/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
+         <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+         <Testimonials/>
+                  <Title subTitle='Contact Us' title='Get in Touch'/>
+                  <Contact/>
         </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,27 @@
+<<<<<<< HEAD
 import React from 'react'
+=======
+import React, { useEffect, useState } from 'react'
+>>>>>>> c0116ce (code added)
 import "./Navbar.css"
 import logo from "../../assets/logo.png"
 
 function Navbar() {
+<<<<<<< HEAD
     return (
         <nav className='container dark-nav'>
+=======
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY> 50 ? setSticky(true) : setSticky(false);
+        })
+    }, [])
+
+    return (
+        <nav className= {`container  ${sticky ? 'dark-nav' : ''}`}>
+>>>>>>> c0116ce (code added)
             <img src={logo} alt="" className='logo' />
             <ul>
                 <li>Home</li>
@@ -12,7 +29,11 @@ function Navbar() {
                 <li>About us</li>
                 <li>Campus</li>
                 <li>Testimonials</li>
+<<<<<<< HEAD
                  <li><button className='btn'>Contact us</button></li>
+=======
+                <li><button className='btn'>Contact us</button></li>
+>>>>>>> c0116ce (code added)
             </ul>
         </nav>
     )
